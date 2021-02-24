49e10a4689371937f528bb5b4ca0288bcf96ba79
Vijay Chaurasiya <vijaychaurasiya911@gmail.com>
2021-02-24 22:47:36 +0530

    Add files via upload

---
+++ loginSystemNodeJSFinal/src/controllers/homePageController.js
@@ -0,0 +1,10 @@
+let handleHelloWorld = async (req, res) => {
+    return res.render("homepage.ejs",{
+        user: req.user
+    });
+   
+};
+
+module.exports = {
+    handleHelloWorld: handleHelloWorld,
+};
